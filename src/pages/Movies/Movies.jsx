48875836cf108bfb927f97f3a9d7dd4b73020e43
lex <savchenko.lex@gmail.com>
2023-10-16 15:11:43 +0300

    Find form was added

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,8 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useParams, useSearchParams } from "react-router-dom";
+import css from "./Movies.module.css";
 
 export default function Movies () {
+    const [query, setQuery] = useSearchParams();
+
+    // useEffect
+
     return <>
-        <p>Movies</p>
+        <div className={css.container}>
+        <h2 className={css.pageTitle}>Find Movies by Title</h2>
+        <form onSubmit={(e)=> {
+                e.preventDefault(); 
+                console.log(e.target.elements[0].value);
+                }
+            }>
+            <input type="text" onChange={(e)=>{console.log(e.target.value)}}></input>
+            <button type="submit">Find</button>
+        </form>
+        </div>
+
         <Outlet />
     </>
 }
