5a885e3710cf528f7d56bbe2a353578f965ee065
lex <savchenko.lex@gmail.com>
2023-10-18 14:57:16 +0300

    page review is done

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -9,11 +9,9 @@ export default function Cast() {
 
   useEffect(() => {
     const endpoint = "/movie/" + movieId + "/credits";
-    console.log(endpoint);
 
     tmdbConnector(endpoint)
       .then((response) => {
-        console.log("credits", response);
         setCast(response.cast);
       })
       .catch((error) => console.error(error));
@@ -22,14 +20,12 @@ export default function Cast() {
   return (
     <>
       <ul className={css.castBox}>
-        {console.log(process.env.PUBLIC_URL + "/draft.svg")}
+        
         {cast.map((actor) => {
           const fullImgUrl = `https://image.tmdb.org/t/p/w200/${actor.profile_path}`;
-          return (
-            <>
-              <li className={css.actorCard}>
+          return (<li key={actor.id} className={css.actorCard}>
                 <h3>{actor.character}</h3>
-                <p>{actor.name}</p>
+                <p>{actor.name}</p> 
                 <div>
                   <img
                     className={css.actorImg}
@@ -42,7 +38,6 @@ export default function Cast() {
                   />
                 </div>
               </li>
-            </>
           );
         })}
       </ul>
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import tmdbConnector from "../../utils/tmdbapi";
+import { useParams } from "react-router-dom";
+
 export default function Reviews (){
+    const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const endpoint = "/movie/" + movieId + "/reviews";
+
+    tmdbConnector(endpoint)
+      .then((response) => {
+        
+        setReviews(response.results);
+      })
+      .catch((error) => console.error(error));
+  }, [movieId]);
+
     return <>
-        <p>This is Reviews</p>
+        {reviews.map((review)=>{
+            return (<div key={review.id}>
+                <h4>{review.author}</h4>
+                <p>{review.content}</p>
+            </div>)
+        })}
     </>
 }
